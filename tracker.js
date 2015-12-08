dd5447106c386cd51ace2ab14e1d6d9fc94f0188
BbDgtl <bbdgtl@gmail.com>
2015-12-08 14:27:54 -0800

    create framework in HTML & begin Javascript

--- tracker.js
+++ tracker.js
@@ -1,9 +1,38 @@
 //creating an array for the photos.
 var kittyPix = [];
 
-function superLuckHappyCuteLilKittenz(photo, path) {
+//superLuckyHappyCuteLilKittenz object.
+function superLuckyHappyCuteLilKittenz(photo, path) {
   this.photo = photo;
   this.path = path;
   this.vote = 0;
   kittyPix.push(this);
+}
+
+//creating a switch case for the tracker
+var tracker = {
+  pic1: 0,
+  pic2: 0
+};
+
+//DOM manipulation, creating "kitty" elements.
+var kitty1 = document.createElement("img");
+var kitty2 = document.createElement("img");
+var temp1 = document.getElementById("temp1");
+var temp2 = document.getElementById("temp2");
+
+//random number generator
+function genRandom() {
+  return Math.floor(Math.random() * kittyPix.length);
+}
+
+//display kitty photos.
+displayKittenz = function() {
+  tracker.pic1 = genRandom();
+  tracker.pic2 = genRandom();
+  pic1.src = kittyPix[tracker.pic1].path;
+};
+
+if (tracker.pic1 === tracker.pic2) {
+
 }
